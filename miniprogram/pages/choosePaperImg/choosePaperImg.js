d6969192178eab61e7f4a9db2fe44f1d77880075
ymhYuhui <1766743814@qq.com>
2020-09-14 16:07:54 +0800

    0914李铭鑫

--- miniprogram/pages/choosePaperImg/choosePaperImg.js
+++ miniprogram/pages/choosePaperImg/choosePaperImg.js
@@ -1,66 +1,85 @@
-// miniprogram/pages/choosePaperImg/choosePaperImg.js
+var app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    imgUrls: [
+      "/images/choosePaperImg/choosePaperChooseImage1.png",
+      "/images/choosePaperImg/choosePaperChooseImage2.png",
+      "/images/choosePaperImg/choosePaperChooseImage1.png",
 
+    
+
+
+    ],
+
+    currentIndex: 0, // 页面swiper的current索引
+    index: 0,
+    flag: true
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  toLeft: function (e) {
+    console.log(this.data.flag)
+    if (!this.data.flag) { // 如果动画还未完成，不执行
+      return
+    } else {
+      // 修改按钮切换不可用状态
+      this.setData({
+        flag: false
+      })
+      var index = this.data.index
+      if (index > 0) {
+        this.setData({
+          currentIndex: index - 1
+        })
+      } else {
+        this.setData({
+          currentIndex: 2
+        })
+      }
+    }
+  },
+  toRight: function (e) {
+    console.log(this.data.flag)
+    if (!this.data.flag) { 
+      // 如果动画还未完成，不执行
+      return
+    } else {
+      // 修改按钮切换不可用状态
+      this.setData({
+        flag: false
+      })
+      var index = this.data.index
+      if (index >= 2) {
+        this.setData({
+          currentIndex: 0
+        })
+      } else {
+        this.setData({
+          currentIndex: index + 1
+        })
+      }
+    }
+  },
+  changeIndex: function (e) { // 切换过程绑定
+    this.setData({
+      index: e.detail.current
+    })
+  },
+  changeFinish: function (e) { // 动画完全完成
+    // 修改按钮切换可用状态
+    this.setData({
+      flag: true
+    })
+  },
+  toEdit: function (opotions) {
+    wx.navigateTo({
+      url: '../editTextImg/editTextImg'
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onSwipertap: function (e) {
+    console.log(e.detail.current)
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
